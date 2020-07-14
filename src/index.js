968ea9de62648b00671a4cda7345ba7771ce7971
nathan-m-schneider-22 <nathan.m.schneider.22@dartmouth.edu>
2020-07-14 12:39:28 -0600

    completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+let seconds = 0;
+
+setInterval(() => {
+  seconds += 1;
+  $('#main').html(`You've been on the page for ${seconds} seconds`);
+}, 1000);
